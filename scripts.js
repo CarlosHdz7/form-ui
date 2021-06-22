49b29b6b66e49bb03ac67147ddb2a15d313e00cb
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-06-21 21:55:47 -0600

    :construction: Adding some validations

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', (event) => {
+
+    //[Objects]
     const TXTName = document.getElementById('TXTName');
     const TXTLastname = document.getElementById('TXTLastname');
     const TXTAge = document.getElementById('TXTAge');
@@ -9,5 +11,50 @@ window.addEventListener('load', (event) => {
     const TXTPassword = document.getElementById('TXTPassword');
     const TXTRepeatPassword = document.getElementById('TXTRepeatPassword');
     const CHKTerms = document.getElementById('CHKTerms');
+    const BTNSignin = document.getElementById('BTNSignin');
+
+    //[Events]
+    BTNSignin.addEventListener('click',(e)=>{
+        e.preventDefault();
+        validateForm();
+    })
+
+    //[Functions]
+    const validateForm = () => {
+
+        if(TXTName.value == ''){
+            TXTName.classList.add("invalid-input");
+        }
+
+        if(TXTLastname.value == ''){
+            TXTLastname.classList.add("invalid-input");
+        }
+
+        if(!isNaN(TXTAge.value)){
+            TXTAge.classList.add("invalid-input");
+        }
+
+        if(TXTPhone.value == ''){
+            TXTPhone.classList.add("invalid-input");
+        }
+
+        if(TXTWebsite.value == ''){
+            TXTWebsite.classList.add("invalid-input");
+        }
+
+        if(TXTEmail.value == ''){
+            TXTEmail.classList.add("invalid-input");
+        }
+
+        if(TXTPassword.value == ''){
+            TXTPassword.classList.add("invalid-input");
+        }
+
+        if(TXTRepeatPassword.value == ''){
+            TXTRepeatPassword.classList.add("invalid-input");
+        }
+
+        
+    }
 
 });
