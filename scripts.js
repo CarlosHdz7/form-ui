dd0d920d172b4dd1ff8e4051cfa6efbc46d61db9
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-06-21 21:25:19 -0600

    :hammer: Adding terms and coditions, Adding some javascript code

---
+++ scripts.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', (event) => {
+    const TXTName = document.getElementById('TXTName');
+    const TXTLastname = document.getElementById('TXTLastname');
+    const TXTAge = document.getElementById('TXTAge');
+    const TXTPhone = document.getElementById('TXTPhone');
+    const TXTWebsite = document.getElementById('TXTWebsite');
+    const RNGExpecience = document.getElementById('RNGExpecience');
+    const TXTEmail = document.getElementById('TXTEmail');
+    const TXTPassword = document.getElementById('TXTPassword');
+    const TXTRepeatPassword = document.getElementById('TXTRepeatPassword');
+    const CHKTerms = document.getElementById('CHKTerms');
+
+});
