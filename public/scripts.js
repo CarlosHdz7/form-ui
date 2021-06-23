e384301d0d06bc27b532f2e7dab703d48590f787
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-06-23 13:18:59 -0600

    :recycle: Improving code syntax

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.addEventListener('load', (event) => {
 
     //[VARIABLES]
@@ -40,16 +42,16 @@ window.addEventListener('load', (event) => {
     const backgroundShadow = document.getElementById('backgroundShadow');
 
     //[EVENTS]
-    btnSignin.addEventListener('click',(e)=>{
+    btnSignin.addEventListener('click', (e) => {
         e.preventDefault();
         validateForm();
     });
 
-    rgnExperience.addEventListener('change',(e) =>{
+    rgnExperience.addEventListener('change', (e) => {
         spnExperience.textContent = e.target.value;
-    })
+    });
 
-    document.addEventListener('keydown',(e)=>{
+    document.addEventListener('keydown', (e) => {
         
         if (preventKeys[e.keyCode]) {
             e.preventDefault(e);
@@ -58,7 +60,7 @@ window.addEventListener('load', (event) => {
         console.log(e);
 
         if(!flagKonamiCode){
-            if(e.key == "ArrowUp"){
+            if(e.key == 'ArrowUp'){
                 flagKonamiCode = true;
                 document.activeElement.blur();
                 createButton(e);
@@ -69,13 +71,12 @@ window.addEventListener('load', (event) => {
             createButton(e);
         }
 
-    })
+    });
 
 
     //[FUNCTIONS]
 
     /*FORM FUNCTIONS*/
-    /*===================================================*/
     const validateForm = () => {
         let errors = [];
 
@@ -91,210 +92,208 @@ window.addEventListener('load', (event) => {
         console.clear();
 
         if(errors.includes(false)){
-            console.log("%c 🚨 Invalid submit","color:red");
+            console.log('%c 🚨 Invalid submit','color:red');
             return;
         }
 
         showSubmittedData();
         
-    }
+    };
 
     const validateName = () => {
 
-        if(isEmptyString(txtName.value)){
-            showWarning(txtName,msgName,"You must provide a name");
+        if(!txtName.value){
+            showWarning(txtName, msgName, 'You must provide a name.');
             return false;
         }
 
         if(!isValidName.test(txtName.value)){
-            showWarning(txtName,msgName,"The name must only contain letters");
+            showWarning(txtName, msgName, 'The name must only contain letters.');
             return false;
         }
 
-        clearWarning(txtName,msgName);
+        clearWarning(txtName, msgName);
         return true;
-    }
+    };
 
     const validateLastName = () => {
-        if(isEmptyString(txtLastName.value)){
-            showWarning(txtLastName,msgLastName,"You must provide a lastname");
+        if(!txtLastName.value){
+            showWarning(txtLastName, msgLastName, 'You must provide a lastname.');
             return false;
         }
 
         if(!isValidName.test(txtLastName.value)){
-            showWarning(txtLastName,msgLastName,"The lastname must only contain letters");
+            showWarning(txtLastName, msgLastName, 'The lastname must only contain letters.');
             return false;
         }
 
-        clearWarning(txtLastName,msgLastName);
+        clearWarning(txtLastName, msgLastName);
         return true;
-    }
+    };
 
     const validateAge = () => {
 
-        if(isEmptyString(txtAge.value)){
-            showWarning(txtAge,msgAge,"You must provide an age");
+        if(!txtAge.value){
+            showWarning(txtAge, msgAge, 'You must provide an age.');
             return false;
         }
 
         if(!(txtAge.value >= 18 && txtAge.value <= 40)){
-            showWarning(txtAge,msgAge,"Age has to be between 18 and 40");
+            showWarning(txtAge, msgAge, 'Age has to be between 18 and 40.');
             return false;
         }
 
         if(!onlyNumbers.test(txtAge.value)){
-            showWarning(txtAge,msgAge,"Invalid age");
+            showWarning(txtAge, msgAge, 'Invalid age.');
             return false;
         }
 
-        clearWarning(txtAge,msgAge);
+        clearWarning(txtAge, msgAge);
         return true;
-    }
+    };
 
-    const validatePhone = () =>{
-        if(isEmptyString(txtPhone.value)){
-            showWarning(txtPhone,msgPhone,"You must provide a phone");
+    const validatePhone = () => {
+        if(!txtPhone.value){
+            showWarning(txtPhone, msgPhone, 'You must provide a phone.');
             return false;
         }
 
         if(!isPhoneNumber.test(txtPhone.value)){
-            showWarning(txtPhone,msgPhone,"You must provide a valid phone");
+            showWarning(txtPhone, msgPhone, 'You must provide a valid phone.');
             return false;
         }
 
-        clearWarning(txtPhone,msgPhone);
+        clearWarning(txtPhone, msgPhone);
         return true;
-    }
+    };
 
     const validateWebSite = () => {
-        if(isEmptyString(txtWebSite.value)){
-            showWarning(txtWebSite,msgWebSite,"You must provide an URL");
+        if(!txtWebSite.value){
+            showWarning(txtWebSite, msgWebSite, 'You must provide an URL.');
             return false;
         }
 
         if(!isvalidUrl.test(txtWebSite.value)){
-            showWarning(txtWebSite,msgWebSite,"You must provide a valid URL");
+            showWarning(txtWebSite, msgWebSite, 'You must provide a valid URL.');
             return false;
         }
 
-        clearWarning(txtWebSite,msgWebSite);
+        clearWarning(txtWebSite, msgWebSite);
         return true;
-    }
+    };
 
     const validateEmail = () => {
-        if(isEmptyString(txtEmail.value)){
-            showWarning(txtEmail,msgEmail,"You must provide an email");
+        if(!txtEmail.value){
+            showWarning(txtEmail, msgEmail, 'You must provide an email.');
             return false;
         }
 
         if(!isValidEmail.test(txtEmail.value)){
-            showWarning(txtEmail,msgEmail,"You must provide a valid email");
+            showWarning(txtEmail, msgEmail, 'You must provide a valid email.');
             return false;
         }
 
-        clearWarning(txtEmail,msgEmail);
+        clearWarning(txtEmail, msgEmail);
         return true;
-    }
+    };
 
     const validatePassword1 = () => {
 
-        if(isEmptyString(txtPassword.value)){
-            showWarning(txtPassword,msgPassword,"You must provide a password");
+        if(!txtPassword.value){
+            showWarning(txtPassword, msgPassword, 'You must provide a password.');
             return false;
         }
 
         if(txtPassword.value.length < 6){
-            showWarning(txtPassword,msgPassword,"You must have at least 6 digits");
+            showWarning(txtPassword, msgPassword, 'You must have at least 6 digits.');
             return false;
         }
 
-        clearWarning(txtPassword,msgPassword);
+        clearWarning(txtPassword, msgPassword);
         return true;
-    }
+    };
 
     const validatePassword2 = () => {
-        if(isEmptyString(txtRepeatPassword.value)){
-            showWarning(txtRepeatPassword,msgRepeatPassword,"You must repeat the password");
+        if(!txtRepeatPassword.value){
+            showWarning(txtRepeatPassword, msgRepeatPassword, 'You must repeat the password.');
             return false;
         }
 
         if(txtPassword.value !== txtRepeatPassword.value){
-            showWarning(txtRepeatPassword,msgRepeatPassword,"Password doesn't match");
+            showWarning(txtRepeatPassword, msgRepeatPassword, 'Password doesn\'t match.');
             return false;
         }
-        clearWarning(txtRepeatPassword,msgRepeatPassword);
+        clearWarning(txtRepeatPassword, msgRepeatPassword);
         return true;
-    }
+    };
 
     const agreeTerms = () => {
         if(!chkTerms.checked){
-            msgTerms.textContent = "You must agree terms and coditions";
-            msgTerms.classList.remove("d-none");
+            msgTerms.textContent = 'You must agree terms and coditions.';
+            msgTerms.classList.remove('d-none');
             return false;
         }
 
-        msgTerms.textContent = "";
-        msgTerms.classList.add("d-none");
+        msgTerms.textContent = '';
+        msgTerms.classList.add('d-none');
         return true;
-    }
+    };
 
-    const showSubmittedData = () =>{
-        console.log("%c ✅ Successfull submit","color:green");
+    const showSubmittedData = () => {
+        console.log('%c ✅ Successfull submit', 'color:green');
         console.table({
-            "Name":txtName.value,
-            "Last Name":txtLastName.value,
-            "Age": txtAge.value,
-            "Phone": txtPhone.value,
-            "Website": txtWebSite.value,
-            "Email": txtEmail.value,
-            "Password": txtPassword.value,
-            "Year's experience": rgnExperience.value
+            'Name':txtName.value,
+            'Last Name':txtLastName.value,
+            'Age': txtAge.value,
+            'Phone': txtPhone.value,
+            'Website': txtWebSite.value,
+            'Email': txtEmail.value,
+            'Password': txtPassword.value,
+            'Year\'s experience': rgnExperience.value,
         });
-    }
-    /*===================================================*/
+    };
 
     /*KONAMI CODE FUNCTIONS*/
-    /*===================================================*/
-    const createButton = (e) =>{
+    const createButton = (e) => {
         switch (e.key) {
-            case "ArrowUp":{
-                addContentButton("🢁");
+            case 'ArrowUp':{
+                addContentButton('🢁');
                 userInputs.push(e.which);
                 updateButtons();
                 break;
             }
             
-            case "ArrowDown":{
-                addContentButton("🢃");
+            case 'ArrowDown':{
+                addContentButton('🢃');
                 userInputs.push(e.which);
                 updateButtons();
                 break;
             }
 
-            case "ArrowLeft":{
-                addContentButton("🢀");
+            case 'ArrowLeft':{
+                addContentButton('🢀');
                 userInputs.push(e.which);
                 updateButtons();
                 break;
             }
 
-            case "ArrowRight":{
-                addContentButton("🢂");
+            case 'ArrowRight':{
+                addContentButton('🢂');
                 userInputs.push(e.which);
                 updateButtons();
                 break;
             }
 
-            case "Escape":{
+            case 'Escape':{
                 clearButtons();
                 break;
             }
 
-            case "Enter":{
-                if(arrayEquals(userInputs,konamiCode)){
-                    console.log("true")
+            case 'Enter':{
+                if(arrayEquals(userInputs, konamiCode)){
+                    console.log('true')
                 }else{
-                    console.log("false");
+                    console.log('false');
                 }
                 clearButtons();
                 break;
@@ -308,21 +307,21 @@ window.addEventListener('load', (event) => {
                 break;
         }
 
-    }
+    };
 
     const addContentButton = (digit) => {
-        let div = document.createElement("DIV");
-        div.classList.add("code-container__button");
+        let div = document.createElement('DIV');
+        div.classList.add('code-container__button');
         div.innerHTML = digit;
         codeContainer.appendChild(div);
-    }
+    };
 
     const clearButtons = () => {
         userInputs = [];
         flagKonamiCode = false;
         backgroundShadow.classList.add('d-none');
         while(codeContainer.firstChild) codeContainer.removeChild(codeContainer.firstChild);
-    }
+    };
 
     const updateButtons = () => {
         if(userInputs.length > 10) {
@@ -330,34 +329,29 @@ window.addEventListener('load', (event) => {
             userInputs.shift();
             return;
         }
-    }
-    /*===================================================*/
+    };
 
-    /*UTILITIES*/
-    /*===================================================*/
-    const isEmptyString = text => (text == "") ? true : false;
-
-    const arrayEquals = (a, b) =>{
+    //[UTILITIES]
+    const arrayEquals = (a, b) => {
         return Array.isArray(a) &&
             Array.isArray(b) &&
             a.length === b.length &&
             a.every((val, index) => val === b[index]);
-    }
+    };
     
-    const clearWarning = (input, message) =>{
-        message.textContent = "";
-        message.classList.add("d-none");
-        input.classList.remove("invalid-input");
-    }
+    const clearWarning = (input, message) => {
+        message.textContent = '';
+        message.classList.add('d-none');
+        input.classList.remove('invalid-input');
+    };
 
-    const showWarning = (input, message,text) =>{
+    const showWarning = (input, message, text) => {
         message.textContent = text;
-        message.classList.remove("d-none");
-        input.classList.add("invalid-input");
-    }
-    /*===================================================*/
+        message.classList.remove('d-none');
+        input.classList.add('invalid-input');
+    };
 
-    //[Settings]
+    //[SETTINGS]
     frmSignIn.reset();
     clearButtons();
 });
